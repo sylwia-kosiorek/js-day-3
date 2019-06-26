7e9077b0710d670a28d638ccfca84af6a450cf4c
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-26 13:49:34 +0200

    Zaczynamy poruszac elementem

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ carElement.style.height = '100px'
 carElement.style.backgroundColor = 'red'
 // Nadanie elementowi wysokosci, szerokosci oraz koloru
 
+carElement.style.position ='absolute'
+carElement.style.left = '0px'
+// Pozycja elementu
+
 const body = document.querySelector('body')
 // Do zmiennej zapisuje referencje, ktora odpowiada utworzonemu elementowi
 
 body.appendChild(carElement)
 // Utworzony child - element wyswietla sie na naszej stronie
 
+let position = 0
+// Zmienna zapisujaca pozycje
 
+setInterval (
+    function () {
+        position = position + 10
+        carElement.style.left = position + 'px'
+    },
+    100
+)
 
+// Wprawienie elelemntu w ruch w interwalach 100ms
