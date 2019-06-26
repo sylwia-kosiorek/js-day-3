029f7b24c73d8f447b0fb90f8074f15bd36e1c45
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-26 14:06:14 +0200

    Further progress with cleaning up the code

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const carElement = document.createElement ('div')
-// Stworzenie elementu w HTML
+const carElement = document.createElement('div')
 const body = document.querySelector('body')
-// Do zmiennej zapisuje referencje, ktora odpowiada utworzonemu elementowi
 let position = 0
-// Zmienna zapisujaca pozycje
-
-carElement.style.width = '100px'
-carElement.style.height = '100px'
-carElement.style.backgroundColor = 'red'
-// Nadanie elementowi wysokosci, szerokosci oraz koloru
-
-carElement.style.position ='absolute'
-carElement.style.left = '0px'
-// Pozycja elementu
-
-body.appendChild(carElement)
-// Utworzony child - element wyswietla sie na naszej stronie
 
 
-setInterval (
-    function () {
-        position = position + 10
-        carElement.style.left = position + 'px' // Funkcja jest tutaj anonimowac
-    },
+function createCar() {
+    carElement.style.width = '100px'
+    carElement.style.height = '100px'
+    carElement.style.backgroundColor = 'red'
+
+    carElement.style.position = 'absolute'
+    carElement.style.left = '0px'
+
+    body.appendChild(carElement)
+}
+
+function move() {
+    position = position + 10
+    carElement.style.left = position + 'px'
+}
+
+createCar()
+
+setInterval(
+    move,
     100
 )
 
-// Wprawienie elelemntu w ruch w interwalach 100ms
