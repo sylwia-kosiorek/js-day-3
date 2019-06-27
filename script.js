98daa33bb572d38795456fbc579a5b6109d4b3a3
Sylwia Kosiorek <sylwiaxkosiorek@gmail.com>
2019-06-27 09:17:16 +0200

    Revert "Braking to 0 m/s not to reverse gear"

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const carElement = document.createElement('div')
 const body = document.querySelector('body')
+const carElement = document.createElement('div')
 const tickDuration = 10
 const maxAcceleration = 10
 
@@ -7,53 +7,55 @@ let velocity = 0
 let acceleration = 0
 let position = 0
 
-
 function applyStylesToCar() {
-    carElement.style.width = '100px'
-    carElement.style.height = '100px'
-    carElement.style.backgroundColor = 'red'
+  carElement.className = 'car-game__car-element'
 
-    carElement.style.position = 'absolute'
-    carElement.style.left = position + 'px'
+  carElement.style.width = '100px'
+  carElement.style.height = '100px'
+  carElement.style.backgroundColor = 'red'
 
-    body.appendChild(carElement)
+  carElement.style.position = 'absolute'
+  carElement.style.left = position + 'px'
+
+  body.appendChild(carElement)
 }
 
 function move() {
-    const time = tickDuration / 1000
-    position = position + velocity * time + (acceleration * time * time) / 2
-    velocity = velocity + acceleration * time
-    carElement.style.left = position + 'px'
+  const time = tickDuration / 1000
+
+  position = position + velocity * time + (acceleration * time * time) / 2
+  velocity = velocity + acceleration * time
+
+  carElement.style.left = position + 'px'
 }
 
 applyStylesToCar()
 
 setInterval(
-    move,
-    tickDuration
+  move,
+  tickDuration
 )
 
 window.addEventListener(
-    'keydown',
-    function (event) {
-      if (event.key === 'a') {
-        acceleration = maxAcceleration
-      }
-      if (event.key === 'b') {
-        acceleration = -maxAcceleration
-      }
+  'keydown',
+  function (event) {
+    if (event.key === 'a') {
+      acceleration = maxAcceleration
     }
-  )
-  
-  window.addEventListener(
-    'keyup',
-    function (event) {
-      if (event.key === 'a') {
-        acceleration = 0
-      }
-      if (event.key === 'b') {
-        acceleration = 0
-      }
+    if (event.key === 'b') {
+      acceleration = -maxAcceleration
     }
-  )
-  // Zatrzymanie klawiszem 'b' added - wykorzystano przyspieszenie ujemne
+  }
+)
+
+window.addEventListener(
+  'keyup',
+  function (event) {
+    if (event.key === 'a') {
+      acceleration = 0
+    }
+    if (event.key === 'b') {
+      acceleration = 0
+    }
+  }
+)
